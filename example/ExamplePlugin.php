cf29e96facec3450490d1e4b5c32dc4780a05fbc
potherca <potherca@gmail.com>
2014-11-22 21:01:04 +0100

    Use ExamplePlugin instead of BasePlugin.

---
+++ example/ExamplePlugin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Potherca\Examples\Composer\Plugins;
+
+/** @noinspection PhpUnnecessaryFullyQualifiedNameInspection */
+class ExamplePlugin extends \Potherca\Composer\Plugins\BasePlugin
+{
+    final public function __call($p_sMethodName, $p_aArguments)
+    {
+        echo '--- CALLED: ' . __CLASS__ . '::' . $p_sMethodName . PHP_EOL;
+        echo '    ARGUMENTS: ('. count($p_aArguments) .')' .PHP_EOL;
+        foreach ($p_aArguments as $t_iIndex => $t_uArgument) {
+            if (is_object($t_uArgument)) {
+                echo ($t_iIndex+1) . '.' . get_class($t_uArgument) . PHP_EOL;
+            } else {
+                echo ($t_iIndex+1) . '.' . var_export($t_uArgument,true) . PHP_EOL;
+            }
+        }
+    }
+}
--- src/Plugins/BasePlugin.php
+++ src/Plugins/BasePlugin.php
@@ -111,18 +111,5 @@ class BasePlugin extends Project implements PluginInterface,  EventSubscriberInt
         return $aEventHandlers;
     }
 
-    final public function __call($p_sMethodName, $p_aArguments)
-    {
-        echo '--- CALLED: ' . __CLASS__ . '::' . $p_sMethodName . PHP_EOL;
-        echo '    ARGUMENTS: ('. count($p_aArguments) .')' .PHP_EOL;
-        foreach ($p_aArguments as $t_iIndex => $t_uArgument) {
-            if (is_object($t_uArgument)) {
-                echo ($t_iIndex+1) . '.' . get_class($t_uArgument) . PHP_EOL;
-            } else {
-                echo ($t_iIndex+1) . '.' . var_export($t_uArgument,true) . PHP_EOL;
-            }
-        }
-    }
-
     ////////////////////////////// UTILITY METHODS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 }
